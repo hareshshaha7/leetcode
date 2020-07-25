639366b68c4c96611dc4f7c422efe65f23d492e4
hareshshaha7 <haresh.shaha7@gmail.com>
2020-07-26 02:34:34 +0530

    Sorted Squares

--- README.md
+++ README.md
@@ -3,5 +3,6 @@
 Learn competitive programming through [leetcode](https://leetcode.com/)
 
 <b>1st program:</b> Array of DVDs <br/>
-<b>2nd program:</b> Find Max Consecutive Ones
-<b>3rd program:</b> Even number of digits
+<b>2nd program:</b> Find Max Consecutive Ones <br/>
+<b>3rd program:</b> Even number of digits <br/>
+<b>4th program:</b> Sorted Squares
---
+++ src/com/haresh/leetcode/array/Program4.java
@@ -0,0 +1,52 @@
+package com.haresh.leetcode.array;
+
+import java.util.Arrays;
+
+/*
+	Given an array of integers A sorted in non-decreasing order, return an array of the squares of each number, also in sorted non-decreasing order.
+
+ 
+	Example 1:
+	
+	Input: [-4,-1,0,3,10]
+	Output: [0,1,9,16,100]
+
+	Example 2:
+	
+	Input: [-7,-3,2,3,11]
+	Output: [4,9,9,49,121]
+	 
+
+	Note:
+	1 <= A.length <= 10000
+	-10000 <= A[i] <= 10000
+	A is sorted in non-decreasing order.
+
+ */
+
+class Solution4 {
+	public int[] sortedSquares(int[] A) {
+		int[] squaredArray = new int[A.length];
+		for (int i = 0; i < A.length; i++) {
+			squaredArray[i] = A[i] * A[i];
+		}
+		Arrays.sort(squaredArray);
+
+		return squaredArray;
+	}
+}
+
+public class Program4 {
+
+	public static void main(String[] args) {
+
+		int[] input = { -7, -3, 2, 3, 11 };
+		Solution4 solution4 = new Solution4();
+		int[] sortedSquares = solution4.sortedSquares(input);
+		for (int square : sortedSquares) {
+			System.out.println(square);
+		}
+
+	}
+
+}
