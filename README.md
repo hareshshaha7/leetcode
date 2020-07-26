ac21799f00b70eb43fdd441dd45d8dffd1b98906
hareshshaha7 <haresh.shaha7@gmail.com>
2020-07-26 21:11:43 +0530

    Duplicate Zeros

--- README.md
+++ README.md
@@ -5,4 +5,5 @@ Learn competitive programming through [leetcode](https://leetcode.com/)
 <b>1st program:</b> Array of DVDs <br/>
 <b>2nd program:</b> Find Max Consecutive Ones <br/>
 <b>3rd program:</b> Even number of digits <br/>
-<b>4th program:</b> Sorted Squares
+<b>4th program:</b> Sorted Squares <br/>
+<b>5th program:</b> Duplicate Zeros <br/>
---
+++ src/com/haresh/leetcode/array/Program5.java
@@ -0,0 +1,68 @@
+package com.haresh.leetcode.array;
+
+import java.util.Arrays;
+
+/*
+	Given a fixed length array arr of integers, duplicate each occurrence of zero, shifting the remaining elements to the right.
+
+	Note that elements beyond the length of the original array are not written.
+	
+	Do the above modifications to the input array in place, do not return anything from your function.
+	
+	 
+	
+	Example 1:
+	
+	Input: [1,0,2,3,0,4,5,0]
+	Output: null
+	Explanation: After calling your function, the input array is modified to: [1,0,0,2,3,0,0,4]
+	Example 2:
+	
+	Input: [1,2,3]
+	Output: null
+	Explanation: After calling your function, the input array is modified to: [1,2,3]
+	 
+	
+	Note:	
+	1. 1 <= arr.length <= 10000
+	2. 0 <= arr[i] <= 9
+ */
+
+class Solution5 {
+
+	public void duplicateZeros(int[] arr) {
+		
+		for (int i = 0; i < arr.length - 1; i++) {
+			if (arr[i] == 0) {
+				for (int j = arr.length - 1; j > i; j--) {
+					arr[j] = arr[j-1];
+				}
+				i++;
+			}
+		}
+
+	}
+	
+	public int shiftArray(int index, int[] arr) {
+		for (int i = arr.length - 1; i > index; i--) {
+			arr[i] = arr[i-1];
+		}
+		return 0;
+	}
+
+}
+
+public class Program5 {
+
+	public static void main(String[] args) {
+
+		int[] input = { 1, 0, 2, 3, 0, 4, 5, 0 };
+		Solution5 solution5 = new Solution5();
+		solution5.duplicateZeros(input);
+		for (int number : input) {
+			System.out.println(number);
+		}
+
+	}
+
+}
