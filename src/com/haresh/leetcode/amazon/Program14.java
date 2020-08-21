8e3373732c20487ff6929fdedac4aa2cdfbe71ae
hareshshaha7 <haresh.shaha7@gmail.com>
2020-08-21 14:10:52 +0530

    removed unused imports

--- src/com/haresh/leetcode/amazon/Program14.java
+++ src/com/haresh/leetcode/amazon/Program14.java
@@ -1,9 +1,7 @@
 package com.haresh.leetcode.amazon;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
