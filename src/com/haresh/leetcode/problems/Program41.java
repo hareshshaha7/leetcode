848aa8e6306757e1add0a97df833c9dddfe8c571
Haresh Shaha <haresh.shaha7@gmail.com>
2021-06-12 12:55:29 +0530

    Removed unused imports

--- src/com/haresh/leetcode/problems/Program41.java
+++ src/com/haresh/leetcode/problems/Program41.java
@@ -5,9 +5,7 @@ package com.haresh.leetcode.problems;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
-import java.util.PriorityQueue;
 
 /*
 	Merge k Sorted Lists
