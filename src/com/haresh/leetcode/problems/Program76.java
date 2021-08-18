b5b36748dc92092db526047707be468915c0c638
Haresh Shaha <haresh.shaha7@gmail.com>
2021-08-18 21:40:19 +0530

    Added explanation

--- src/com/haresh/leetcode/problems/Program76.java
+++ src/com/haresh/leetcode/problems/Program76.java
@@ -10,10 +10,18 @@ package com.haresh.leetcode.problems;
 	Given the head of a linked list, rotate the list to the right by k places.
 	
 	Example 1:
+					1 -> 2 -> 3 -> 4 -> 5
+		Rotate 1	5 -> 1 -> 2 -> 3 -> 4
+		Rotate 2	4 -> 5 -> 1 -> 2 -> 3	
 	Input: head = [1,2,3,4,5], k = 2
 	Output: [4,5,1,2,3]
 	
 	Example 2:
+					0 -> 1 -> 2
+		Rotate 1	2 -> 0 -> 1
+		Rotate 2	1 -> 2 -> 0
+		Rotate 3	0 -> 1 -> 2
+		Rotate 4	2 -> 0 -> 1
 	Input: head = [0,1,2], k = 4
 	Output: [2,0,1]
 	
